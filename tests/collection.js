4c328222922bc7d57bca744db37af18bd84cf816
James Hiscock <james@small.mu>
2014-12-29 14:05:08 +1100

    Collection tests.
    Refs #3.

--- tests/collection.js
+++ tests/collection.js
@@ -1,7 +1,17 @@
+var Collection = require('../collection')
+var Model = require('../model')
+
 module.exports = function(test) {
-    test('Collection instantiation', function(t) {
-        t.plan(1)
-        t.ok('ok')
+    test('Collection utilities', function(t) {
+        t.plan(3)
+
+        var c = new Collection([{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }])
+        var second = c.get(2)
+        var third = c.get(3)
+        t.notEqual(c.models, c.all(), 'Return of .all() is referentially different to .models')
+        t.equal(c.before(third), second, 'Before model')
+        t.equal(c.after(second), third, 'After model')
+
         t.end()
     })
 }
